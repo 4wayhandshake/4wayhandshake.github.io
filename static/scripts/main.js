a124fd9f6e889a366204e2007092dc8bd1c3e45a
4wayhandshake <timbo.in.space@proton.me>
2023-07-03 10:34:02 +0300

    Scrolls properly and only uses h1, h2, and h3 now

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -26,11 +26,16 @@ function clearAllActiveNavItems() {
 
 window.addEventListener('DOMContentLoaded', () => {
 
+    // Hugo's Table of Contents only tracks the largest three heading types
+    const headingTypes = ['h1','h2','h3'];
+
     const scrollableElement = document.querySelector('#toc');
     if (!scrollableElement) return;
 
 	const observer = new IntersectionObserver(elements => {
-        elements.forEach(ele => {
+        elements.filter( ele => {
+            return headingTypes.includes(ele.target.tagName.toLowerCase())
+        }).forEach(ele => {
             const id = ele.target.getAttribute('id');
     		if (ele.intersectionRatio > 0) {
                 console.log(`Setting active for #${id}`);
@@ -44,13 +49,12 @@ window.addEventListener('DOMContentLoaded', () => {
                         behavior: "smooth",
                     });
                 }
-
     		}
         });
 	});
 
 	// Track all headings that have an `id` applied
-    ['h1','h2','h3','h4','h5','h6'].forEach((headingType) => {
+    headingTypes.forEach((headingType) => {
         document.querySelectorAll(`${headingType}[id]`).forEach((headingElement) => {
             observer.observe(headingElement);
         });
